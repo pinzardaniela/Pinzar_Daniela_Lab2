dc766f2932ce14221c63d0abd338413f03ce8ad6
pinzardaniela@yahoo.com <pinzardaniela@yahoo.com>
2020-10-15 22:09:04 +0300

    Add project files.

---
+++ Pinzar_Daniela_Lab2/Data/LibraryContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pinzar_Daniela_Lab2.Models;
+
+namespace Pinzar_Daniela_Lab2.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) :
+base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
+    }
+}
