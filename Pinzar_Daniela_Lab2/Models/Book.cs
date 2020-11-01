bcd15b3f8ae5d2b2467f05a7710859bee55d8fd9
DANIELA PÎNZAR <daniela.pinzar@stud.ubbcluj.ro>
2020-11-01 22:11:33 +0200

    Laborator 4

--- Pinzar_Daniela_Lab2/Models/Book.cs
+++ Pinzar_Daniela_Lab2/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,10 @@ namespace Pinzar_Daniela_Lab2.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         public ICollection<Order> Orders { get; set; }
+        public ICollection<PublishedBook> PublishedBooks { get; set; }
     }
 }
