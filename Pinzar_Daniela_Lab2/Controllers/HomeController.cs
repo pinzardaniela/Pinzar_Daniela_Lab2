b2dee057e877f77909065311a54a12aad71b0141
DANIELA PÎNZAR <daniela.pinzar@stud.ubbcluj.ro>
2020-11-25 19:48:26 +0200

    Lab_8

--- Pinzar_Daniela_Lab2/Controllers/HomeController.cs
+++ Pinzar_Daniela_Lab2/Controllers/HomeController.cs
@@ -52,5 +52,10 @@ namespace LibraryModel.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
     }
 }
